159c9eccb49bfb5b3648d9976186e4d562897fdf
kuarisu <kuarisuran@gmail.com>
2016-03-01 14:19:05 +0100

    Ajout d'un script pour l'UI du Menu, mis en stand-by à cause du non multijoueur

---
+++ Assets/Script/ManagerButtonsMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManagerButtonsMenu : MonoBehaviour {
+	
+        //Lorsque le bouton Host est appuyé. La fonction permet de loader la scène de HUD de connection avec tous les paramètres nécessaires au joueur qui host.
+	public void HostPressed() {
+	
+    //Zone réservée au code nécessaire au joueur qui host
+
+
+	}
+
+        //Lorsque le bouton Join est appuyé. La fonction permet de loader la scène de HUD de connection avec tous les paramètres nécessaires au joueur qui join.
+    public void JoinPressed()
+    {
+
+        //Zone réservée au code nécessaire au joueur qui join
+        Application.LoadLevel(1);
+    
+    }
+
+        //Lorsque le bouton Controls est appuyé. Affiche un schéma des commandes.
+    public void ControlsPressed()
+    {
+
+    }
+}
