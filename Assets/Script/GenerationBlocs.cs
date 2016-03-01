a6172b1acadc828ee784fe6906f7af9c497b8cd3
kuarisu <kuarisuran@gmail.com>
2016-03-01 15:29:39 +0100

    Première version de la génération de blocs.

---
+++ Assets/Script/GenerationBlocs.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenerationBlocs : MonoBehaviour {
+
+    public GameObject blocLDTest;       //Récupère l'asset "Test_BlocLD".
+
+	void Start () {
+
+        StartCoroutine(spawnBlocs());
+	}      
+
+    IEnumerator spawnBlocs()
+    {
+        Debug.Log("dans la coroutine");
+        float _height = blocLDTest.GetComponent<Renderer>().bounds.size.y;
+        int _heightNb = 1;
+        int _blocNb = 1;
+        float _time = 3f;
+
+        while (true)
+        {
+            Debug.Log("dans le while");
+            GameObject bloc = Instantiate(blocLDTest, new Vector3(0,_height * _heightNb, 0), Quaternion.Euler(90, 0, 0)) as GameObject;
+            bloc.name = "bloc_" + _blocNb;
+            _heightNb++;
+            yield return new WaitForSeconds(_time);
+        }
+    }
+	
+}
