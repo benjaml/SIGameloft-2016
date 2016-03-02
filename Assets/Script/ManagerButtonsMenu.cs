90ba6010da9f5b381696498a018d6b4308698810
kuarisu <kuarisuran@gmail.com>
2016-03-02 14:06:54 +0100

    Base des différents UI (Menu - InGame (Start/Deaht) - Credit)

--- Assets/Script/ManagerButtonsMenu.cs
+++ Assets/Script/ManagerButtonsMenu.cs
@@ -1,26 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class ManagerButtonsMenu : MonoBehaviour {
 	
-        //Lorsque le bouton Host est appuyé. La fonction permet de loader la scène de HUD de connection avec tous les paramètres nécessaires au joueur qui host.
-	public void HostPressed() {
-	
-    //Zone réservée au code nécessaire au joueur qui host
-
-
-	}
-
-        //Lorsque le bouton Join est appuyé. La fonction permet de loader la scène de HUD de connection avec tous les paramètres nécessaires au joueur qui join.
+        //When "Play" is pressed, this fonction load the game scene (index 1).
     public void JoinPressed()
     {
-
-        //Zone réservée au code nécessaire au joueur qui join
-        Application.LoadLevel(1);
-    
+        SceneManager.LoadScene(1);  //Loading the scene of the game (index 1)
     }
 
-        //Lorsque le bouton Controls est appuyé. Affiche un schéma des commandes.
+        //When "Controls" is pressed, a scheme of the controls shows up on the current scene.
     public void ControlsPressed()
     {
 
