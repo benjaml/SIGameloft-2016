cd77c83dfda034d05265577add7afd0a7df50f06
kuarisu <kuarisuran@gmail.com>
2016-03-03 11:55:55 +0100

    mouvement des collectible derrière fonctionne.

--- Assets/Script/CollectibleMovement.cs
+++ Assets/Script/CollectibleMovement.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CollectibleMovement : MonoBehaviour
 {
     public Transform target;            // The position that that camera will be following.
-    public float smoothing = 5f;        // The speed with which the camera will be following.
+    public float smoothing = 1f;        // The speed with which the camera will be following.
 
     Vector3 offsetPos;                     // The initial offset from the target.
     //Vector3 offsetRot;
@@ -13,17 +13,20 @@ public class CollectibleMovement : MonoBehaviour
     {
         // Calculate the initial offset.
         offsetPos = transform.position - target.position;
-        //offsetRot = transform.eulerAngles - target.eulerAngles;
+
     }
 
     void FixedUpdate()
     {
         // Create a postion the camera is aiming for based on the offset from the target.
-        Vector3 targetCamPos = target.position + offsetPos;
+        Vector3 targetCamPos = new Vector3(target.position.x, target.position.y, target.position.z) + offsetPos;
 
         // Smoothly interpolate between the camera's current position and it's target position.
         transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
 
+         
+
+        //Keep the same rotation than the target
         transform.eulerAngles = target.eulerAngles;
     }
 }
