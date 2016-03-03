2c400fc2ce09f66a54c0e582cf43a0cfbbe9606a
benjaml <b.lefevre@rubika-edu.com>
2016-03-03 15:04:52 +0100

    I got something cool but I always got some weird things while turning around the helix, and the distance from center to player doesn't work

---
+++ Assets/PlayerDetector.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDetector : MonoBehaviour
+{
+
+    public float cylinderRadius = 2.5f;
+    public float distanceFromCenter = 5f;
+
+    //Player Movement reference
+    public PlayerMovement pm;
+    public LayerMask layer;
+
+    
+
+    // Use this for initialization
+    void Start ()
+	{
+	    //pm = transform.parent.GetComponent<PlayerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    RaycastHit hit;
+        /* je fait un raycast vers le bas du joueur pour rentrer en collision avec la spirale
+         * Une fois que j'ai le point de collision avec la spirale, je récupère la normal au point de collision
+         * Pour avoir le centre de la spirale, je prend le point de collision ou je retire la normal * le radius du tube*/
+        if (Physics.Raycast(transform.position, -transform.up, out hit, layer))
+        {
+            Debug.DrawLine(transform.position, transform.position-transform.up * 10.0f, Color.red);
+            if (hit.transform.tag == "floor")
+            {
+                Debug.Log("YEAY");
+                return;
+            }
+            Debug.Log("I found something");
+
+        }
+        else
+        {
+            Debug.Log("I can't see s**t");
+            return;
+        }
+        
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float cylinderRadius = 2.5f;
+    public float distanceFromCenter = 5f;
     public float speed = 6.0F;
     public float turnSpeed = 3.0F;
     public float jumpSpeed = 8.0F;
@@ -14,8 +15,10 @@ public class PlayerMovement : MonoBehaviour
     //position et rotation que je personnage devrais avoir en fin de déplacement
     private Vector3 targetPosition;
     private Quaternion targetRotation;
+    private Quaternion lookRotation;
     //position du centre de gravité
     public Vector3 gravityCenter;
+    public Vector3 secondGravityCenter;
     // direction de la force de gravité
     public Vector3 gravityDirection;
     // vector3 utilisé pour le déplacement
@@ -34,24 +37,33 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        isGrounded = distanceFromCenter < 5.0f ? true : false;
+
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) < 0.3f && Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.3f)
+            return;
+
         // update gravity center
-        RaycastHit hit;
+            RaycastHit hit;
         /* je fait un raycast vers le bas du joueur pour rentrer en collision avec la spirale
          * Une fois que j'ai le point de collision avec la spirale, je récupère la normal au point de collision
-         * Pour avoir le centre de la spirale, je prend le point de collision ou je retire la normal * le radius du tube*/ 
+         * Pour avoir le centre de la spirale, je prend le point de collision ou je retire la normal * le radius du tube*/
         if (Physics.Raycast(transform.position, -transform.up, out hit))
         {
             //Debug.DrawLine(transform.position, transform.position+transform.up * 10, Color.red);
             if (hit.transform.tag == "floor")
             {
                 //Debug.DrawLine(hit.point, hit.point+hit.normal*10, Color.green);
-                gravityCenter = hit.point - hit.normal*hit.transform.localScale.z* cylinderRadius;
+                gravityCenter = hit.point - hit.normal*hit.transform.localScale.z*cylinderRadius;
                 // petite méthode magique pour faire des rotation propre tu met la direction que tu veux changer et celle que tu veux et il la met bien
                 // le soucis de transform.up = (transform.position-gravityCenter).normalized, c'est qu'il te donne la bonne orientation mais tu as pas la bonne transform.rotation
                 // tu peux te retrouver a regarder dans une toute autre direction
-                targetRotation = Quaternion.FromToRotation(transform.up, (transform.position-gravityCenter).normalized );
+                targetRotation = Quaternion.FromToRotation(transform.up, (transform.position - gravityCenter).normalized);
             }
 
+        }
+        else
+        {
+            Debug.Log("I can't see s**t");
         }
 
         //update the player forward direction with the spiral
@@ -64,30 +76,24 @@ public class PlayerMovement : MonoBehaviour
 
             if (hit.transform.tag == "floor")
             {
-                Vector3 secondGravityCenter = hit.point - (hit.normal *cylinderRadius);
+                secondGravityCenter = hit.point - (hit.normal *cylinderRadius);
                 
                 // permet de voir les points de gravité
-                /*Debug.DrawLine(hit.point, hit.normal, Color.green);
-                Debug.DrawLine(gravityCenter, gravityCenter + Vector3.up * 5, Color.red);
-                Debug.DrawLine(gravityCenter, gravityCenter - Vector3.up * 5, Color.red);
-                Debug.DrawLine(gravityCenter, gravityCenter + Vector3.right * 5, Color.red);
-                Debug.DrawLine(gravityCenter, gravityCenter - Vector3.right * 5, Color.red);
-                Debug.DrawLine(secondGravityCenter, secondGravityCenter + Vector3.up*5,Color.red);
-                Debug.DrawLine(secondGravityCenter, secondGravityCenter - Vector3.up*5, Color.red);
-                Debug.DrawLine(secondGravityCenter, secondGravityCenter + Vector3.right*5, Color.red);
-                Debug.DrawLine(secondGravityCenter, secondGravityCenter - Vector3.right*5,Color.red);*/
+                Debug.DrawLine(hit.point, hit.normal, Color.green);
+                DebugPoint(gravityCenter, Color.red);
+                DebugPoint(secondGravityCenter, Color.red);
                 // Idem que au dessus, avec le (*=) pour combiner les 2 quaternions
-                targetRotation *= Quaternion.FromToRotation(transform.forward, (secondGravityCenter - gravityCenter).normalized);
+                lookRotation = Quaternion.FromToRotation(transform.forward, (secondGravityCenter - gravityCenter).normalized);
             }
         }
         // on applique les transformation de rotation a la rotation actuelle
         targetRotation *= transform.rotation;
-        float tmpGrav = gravity;
+        //float tmpGrav = gravity;
         gravityDirection = gravityCenter - transform.position;
-        gravityDirection.Normalize();
+        //gravityDirection.Normalize();
         if (isGrounded)
         {
-            tmpGrav *= 0.1f;
+            //tmpGrav *= 0.1f;
             // avance sur le tube avec la variabe speed
             moveDirection = Input.GetAxisRaw("Vertical")*transform.forward*speed;
             // tourne autour du tube avec la variabe turnSpeed
@@ -97,16 +103,30 @@ public class PlayerMovement : MonoBehaviour
 
         }
         // on ajoute la gravité au déplacement
-        moveDirection += tmpGrav*Time.deltaTime*gravityDirection;
+        //moveDirection += tmpGrav*Time.deltaTime*gravityDirection;
         targetPosition = (transform.position + (moveDirection*Time.deltaTime));
         ApplyMovement();
     }
     
     void ApplyMovement()
     {
+        DebugPoint(targetPosition, Color.blue);
+        DebugPoint(gravityCenter + (targetPosition - gravityCenter).normalized*distanceFromCenter, Color.black);
+        //Debug.DrawLine(gravityCenter, gravityCenter + (targetPosition - gravityCenter).normalized*distanceFromCenter,Color.yellow);
+        transform.position = Vector3.Lerp(transform.position,gravityCenter + (targetPosition - gravityCenter).normalized*distanceFromCenter,0.1f);
         // on applique les modification de position et rotation en smooth
-        rb.MovePosition(Vector3.Lerp(transform.position, targetPosition, 0.3f));
-        rb.MoveRotation( Quaternion.Slerp(transform.rotation, targetRotation, 0.3f));
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, 0.1f);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 0.1f);
+        Debug.DrawLine(transform.position, transform.position + (secondGravityCenter - gravityCenter).normalized*10.0f,Color.red);
+        transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation*Quaternion.FromToRotation(transform.forward, (secondGravityCenter - gravityCenter).normalized), 0.01f);
+    }
+
+    void DebugPoint(Vector3 position, Color color)
+    {
+        Debug.DrawLine(position, position + Vector3.up * 5, color);
+        Debug.DrawLine(position, position - Vector3.up * 5, color);
+        Debug.DrawLine(position, position + Vector3.right * 5, color);
+        Debug.DrawLine(position, position - Vector3.right * 5, color);
     }
     
     void OnCollisionEnter(Collision col)
