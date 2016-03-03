023a479add48bea25eb81f8e10aa5ffc0fe810f1
kuarisu <kuarisuran@gmail.com>
2016-03-03 11:03:42 +0100

    Premier test de déplacement des collectible une fois récupérés

---
+++ Assets/Script/CollectibleMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleMovement : MonoBehaviour
+{
+    public Transform target;            // The position that that camera will be following.
+    public float smoothing = 5f;        // The speed with which the camera will be following.
+
+    Vector3 offsetPos;                     // The initial offset from the target.
+    //Vector3 offsetRot;
+
+    void Start()
+    {
+        // Calculate the initial offset.
+        offsetPos = transform.position - target.position;
+        //offsetRot = transform.eulerAngles - target.eulerAngles;
+    }
+
+    void FixedUpdate()
+    {
+        // Create a postion the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos = target.position + offsetPos;
+
+        // Smoothly interpolate between the camera's current position and it's target position.
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+        transform.eulerAngles = target.eulerAngles;
+    }
+}
