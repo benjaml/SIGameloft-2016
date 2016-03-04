2243dd0d00752ba7413b235193f8fc52d84b45b7
kuarisu <kuarisuran@gmail.com>
2016-03-04 20:22:25 +0100

    Lorsque le joueur entre dans un trigger, il perd ses collectibles ( seulement dans la liste), et en fonction de leur nombre la colère du dragon diminue (à tweakeeeer)

---
+++ Assets/Scripts/GameManagerWrath.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerWrath : MonoBehaviour {
+
+    public GameObject player;
+    [Range(0, 500)]
+    public int wrath;
+    private int wrathLast = -1;
+
+    public int numberOfCollectibles;
+
+    //Call this function and delete the Start when you want to start the game
+    //public void startGame ()
+    //{
+    //    StartCoroutine(wrathAugmentation());
+    //}
+    
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start ()
+    {
+        StartCoroutine(wrathAugmentation());
+    }
+
+    void Update ()
+    {
+        if (wrathLast != wrath)
+        {
+            wrathLast = wrath;
+            Debug.Log(wrath);
+        }
+    }
+
+    public void wrathManaging()
+    {
+        if (numberOfCollectibles <= 0)
+            return;
+        else if (numberOfCollectibles <= 5)
+            wrath -= 5;
+
+        else if (numberOfCollectibles <= 10)
+            wrath -= 2;
+
+        else if (numberOfCollectibles <= 15)
+            wrath -= 3;
+
+        else if (numberOfCollectibles <= 20)
+            wrath -= 4;
+    }
+
+    IEnumerator wrathAugmentation()
+    {
+        while (true)
+        {
+            wrath++;
+            yield return new WaitForSeconds(0.5f);
+        }
+
+    }
+
+}
--- Assets/Scripts/PlayerBellCollision.cs
+++ Assets/Scripts/PlayerBellCollision.cs
@@ -11,16 +11,35 @@ public class PlayerBellCollision : MonoBehaviour {
     //Selon leurs nombres [int] et via un système de paliers [switch ?] on diminue la jauge du de colère du dragon [int] (à récupérer sur un gameManager externe ?) 
     //On clear la liste + FX de clear si besoin. 
 
+    //Variable to stock the gameManagerWrath
+    public GameObject gameManagerWrath;
 
-    //Liste provisoire pour les pétales:
-    private List<GameObject> listProvisoireCollectible = new List<GameObject>();
+    //temporary list of collectible:
+    public List<GameObject> listProvisoireCollectible = new List<GameObject>();
+
+    //Variable to stock the number of collectibles:
+    private int numberOfCollectibles;
 
 
     void OnTriggerEnter (Collider col)
     {
         if (col.tag == "bell")
         {
-           
+            //Start animation, fx and sound
+            numberOfCollectibles = listProvisoireCollectible.Count;
+            Debug.Log(numberOfCollectibles);
+
+            GameManagerWrath wrathManagmementFunction = gameManagerWrath.GetComponent<GameManagerWrath>();
+
+            //Set the lenght of the list as the number of collectibles
+            wrathManagmementFunction.numberOfCollectibles = numberOfCollectibles;
+
+            //Start the function to reduce the wrath gauge of the dragon
+            wrathManagmementFunction.wrathManaging();
+
+
+            //Clear the list with fx
+            listProvisoireCollectible.Clear();
         }
     }
 }
