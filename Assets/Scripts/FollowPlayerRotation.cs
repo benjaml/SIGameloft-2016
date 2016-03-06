b7fb1a5944cf6a8a4c5a858e2bef7d3ab29cb456
Windywyll <appleteensee@yahoo.fr>
2016-03-06 19:19:37 +0100

    bell qui tourne comme y faut

---
+++ Assets/Scripts/FollowPlayerRotation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayerRotation : MonoBehaviour {
+
+    public Transform Player;
+
+    private Vector3 _rot;
+
+	// Use this for initialization
+	void Start () {
+        _rot = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.eulerAngles = new Vector3(Player.eulerAngles.z - 90, _rot.y, _rot.z);
+
+	}
+}
