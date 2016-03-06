a0a3e73508db23b6d832ee7c8e6e9209fd0538eb
Mikozouzou <a.adeline@K003-5017.etudiant.rubika-edu.com.local>
2016-03-06 21:02:28 +0100

    Rework the menu in only one scene with both mouse and gamepad inputs.

---
+++ Assets/Script/AnimationManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationManager : MonoBehaviour {
+
+    public Animator m_MenuAnimator;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Main_To_Options()
+    {
+        m_MenuAnimator.SetTrigger("Main_To_Options");
+    }
+
+    public void Options_To_Main()
+    {
+        m_MenuAnimator.SetTrigger("Options_To_Main");
+    }
+}
