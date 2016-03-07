7139efaf254efb0f9dbac9a82cb0ec9720378e00
kuarisu <kuarisuran@gmail.com>
2016-03-07 16:09:24 +0100

    Collision + ralentissement fonctionnels (prochaine étape: rajout de screenshake et de vibration)

--- Assets/Scripts/BlocSpeedModifier.cs
+++ Assets/Scripts/BlocSpeedModifier.cs
@@ -3,30 +3,64 @@ using System.Collections;
 
 public class BlocSpeedModifier : MonoBehaviour {
 
-    private float modifierSpeed = 2;
+    public float reduceSpeed = 0.6f;
+    public float accelerationTime;
+    public int lowSpeedTime = 5;
+    private float stockBaseSpeed;
+    private float stockMaxSpeed;
+    PlayerMovement playerMovementScript; 
 
     void OnTriggerEnter (Collider col)
     {
         if (col.tag == "Player")
         {
-            PlayerMovement Script = col.gameObject.transform.parent.GetComponent<PlayerMovement>();
-            gameObject.GetComponent<BoxCollider>().enabled = false;
-            Script.reduceCurrentSpeed(modifierSpeed);
-            StartCoroutine(destructionBloc());
+            playerMovementScript = col.gameObject.transform.parent.GetComponent<PlayerMovement>();
+            stockBaseSpeed = playerMovementScript.baseSpeed;
+            playerMovementScript.baseSpeed = playerMovementScript.baseSpeed * reduceSpeed;
+            Debug.Log(playerMovementScript.baseSpeed * reduceSpeed);
+            stockMaxSpeed = playerMovementScript.MaxSpeed;
+            playerMovementScript.MaxSpeed = playerMovementScript.MaxSpeed * reduceSpeed;
+            StartCoroutine(reducingSpeed());
+            //StartCoroutine(destructionBloc());
         }
     }
 
-    IEnumerator destructionBloc()
+    IEnumerator reducingSpeed()
     {
+        Debug.Log("hello");
         int _tmp = 0;
-        while (_tmp < 20)
+        while (_tmp < lowSpeedTime)
         {
             _tmp++;
-            yield return new WaitForEndOfFrame();
+            yield return new WaitForSeconds(0.1f);
         }
-        Destroy(gameObject);
+        if (_tmp >= lowSpeedTime)
+        {
+            while (playerMovementScript.MaxSpeed < stockMaxSpeed)
+            {
+                Debug.Log(playerMovementScript.MaxSpeed);
+                playerMovementScript.baseSpeed += accelerationTime;
+                playerMovementScript.MaxSpeed += accelerationTime;
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+        Debug.Log(stockMaxSpeed);
+        playerMovementScript.baseSpeed= stockBaseSpeed;
+        playerMovementScript.MaxSpeed = stockMaxSpeed;
         yield return null;
-
     }
+
+    //IEnumerator destructionBloc()
+    //{
+    //    int _tmp = 0;
+    //    while (_tmp < 20)
+    //    {
+    //        _tmp++;
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //    Destroy(gameObject);
+    //    yield return null;
+
+    //}
 
 }
