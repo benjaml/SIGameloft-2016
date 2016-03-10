c76eafe45e2c677cfdb9764f60990d2a9a6d5915
benjaml <b.lefevre@rubika-edu.com>
2016-03-10 16:37:04 +0100

    Premiers test de loading screen

---
+++ Assets/Script/loadingScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class loadingScreen : MonoBehaviour {
+
+	public GameObject background;
+	public GameObject progressBar;
+	private int loadProgress = 0;
+
+	// Use this for initialization
+	void Start () {
+		background.SetActive (false);
+		progressBar.SetActive (false);
+	}
+	
+
+	public void load(string level){
+		StartCoroutine(DisplayLoadingScreen(level));
+	}
+
+	IEnumerator DisplayLoadingScreen(string level){
+
+		background.SetActive (true);
+		progressBar.SetActive (true);
+		
+		AsyncOperation async = Application.LoadLevelAsync (level);
+		while (!async.isDone) {
+			progressBar.GetComponent<Image>().fillAmount = async.progress;
+			yield return null;
+		}
+	}
+}
