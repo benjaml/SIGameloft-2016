66e9ee30a50152adf96741a61e58a6da60c699a6
Windywyll <appleteensee@yahoo.fr>
2016-03-10 22:32:54 +0100

    prefab de fresque

---
+++ Assets/FrescoFollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrescoFollowPlayer : MonoBehaviour
+{ 
+
+    public Transform Player;
+
+    private Vector3 _rot;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rot = transform.rotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //transform.eulerAngles = new Vector3(Player.eulerAngles.z - 90, _rot.y, _rot.z);
+        //transform.eulerAngles = new Vector3(_rot.x, Player.eulerAngles.z, _rot.z);
+        transform.eulerAngles = new Vector3(_rot.x, _rot.y, -1 * Player.eulerAngles.z);
+
+        //transform.rotation = Player.rotation;
+
+    }
+}
